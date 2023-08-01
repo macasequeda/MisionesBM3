aac4e84ba2f7d6ce4b3e209a21e580add3b5133e
macasequeda <macasequeda26@gmail.com>
2023-07-31 21:30:13 -0500

    Modificar commits creados

--- operador-ternario.js
+++ operador-ternario.js
@@ -1,6 +1,3 @@
 let number = 8
-if(number === 8) {
-    console.log("El número es igual a " + number)
-} else if(number !== 8) {
-    console.log("No es " + 8)
-}
+number = 8 
+    console.log("El número es igual a " + 8) ; console.log("No es " + 8); 
